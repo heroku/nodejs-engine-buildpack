e69a6a566a389769cbdd12c3ebd404a435050771
Jesse Brown <jabrown85@gmail.com>
2020-01-29 16:32:10 -0600

    Ensure we provide and require node
    In order for this buildpack to be used alone, it must require node and provide node so that the detection phase doesn't fail with the "provides unused node" error. This will still satisfy the requirements from other buildpacks that require node.

--- shpec/detect_shpec.sh
+++ shpec/detect_shpec.sh
@@ -29,4 +29,22 @@ describe "lib/detect.sh"
       assert equal "$?" 0
     end
   end
+
+  describe "write_to_build_plan"
+    it "writes node for [[provides]] and [[requires]]"
+      project_dir=$(create_temp_project_dir)
+      touch "$project_dir/buildplan.toml"
+      write_to_build_plan "$project_dir/buildplan.toml"
+      actual=`cat $project_dir/buildplan.toml`
+      echo $actual
+      expected=`cat <<EOF
+  [[provides]]
+  name = "node"
+
+  [[requires]]
+  name = "node"
+EOF`
+      assert equal "$actual" "$expected"
+    end
+  end
 end
